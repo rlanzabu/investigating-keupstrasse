240bf001612d5d84871237a36cc7b4d6674d69ca
Raffaella Carolina Lanza Bugueño <raffaella_carolina.lanza_bugueno@smail.th-koeln.de>
2026-01-03 20:18:51 +0100

    testing initial gsap setting with free svg

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("JavaScript is working!");
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    gsap.to("#mySvg", {
+        transformOrigin: "50% 50%",
+        rotation: 360,
+        duration: 2
+    });
+});
